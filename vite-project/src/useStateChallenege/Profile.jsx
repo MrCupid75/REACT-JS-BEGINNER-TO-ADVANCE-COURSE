ba56656ea6a36af468dcba596252534935b93970
Beast <mjnuvor@gmail.com>
2025-03-15 22:50:45 +0000

    change profile component to use input fields

--- vite-project/src/useStateChallenege/Profile.jsx
+++ vite-project/src/useStateChallenege/Profile.jsx
@@ -4,9 +4,21 @@ const Profile = () => {
 
     const [user, setUser] = useState({name: "Beast", age: 45})
 
-    const handleClick = () => setUser(() => {
-        return {...user, name: "Raw", age: 23}
-    })
+    const [inputValueName, setInputValueName] = useState("")
+    const [inputValueAge, setInputValueAge] = useState("")
+
+    const handleChangeName =(event) => setInputValueName(event.target.value)
+    const handleChangeAge =(event) => setInputValueAge(event.target.value)
+
+    const handleClickAge = () => {
+        setUser({...user, age:inputValueAge})
+        setInputValueAge("")
+    }
+
+    const handleClickName = () => {
+        setUser({...user, name:inputValueName})
+        setInputValueName("")
+    }
 
   return (
     <div>
@@ -14,7 +26,10 @@ const Profile = () => {
         <h4>Name: {user.name}</h4>
         <h4>Age: {user.age}</h4>
 
-        <button onClick={handleClick}>Update</button>
+        <input type="text" placeholder='Enter something' value={inputValueName} onChange={handleChangeName}/>
+        <button onClick={handleClickName}>Update name</button>
+        <input type='number' placeholder='Enter something' value={inputValueAge} onChange={handleChangeAge}/>
+        <button onClick={handleClickAge}>Update Age</button>
     </div>
   )
 }
